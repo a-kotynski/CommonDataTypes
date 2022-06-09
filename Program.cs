903d3ff3e9c3cca13d5ff6c41463c432e7fae37b
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-09 06:44:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            */ /**/ /* forward slash - star - star - forward slash */
+
+            /*
+            //variables
+            int number;
+            //int is a type, number is an identifier
+            int Number = 1; //variable with an assigned value
+            //Console.WriteLine(number = 1);
+            //Console.WriteLine("{0}",number=1);
+
+            const float Pi = 3.14f;
+            //for variables use Camel Case
+            //for constants Pascal Case
+            */
+
+            //Integral Numbers:     .Net Type:      Bytes:      Range: 
+            //byte                  Byte            1           0 to 255
+            //short                 Int16           2           -32768 to 32767
+            //int                   Int32           4           -2.1B to 2.1B
+            //long                  Int64           8           ??? large tho
+
+            //Real Numbers:         .Net Type:      Bytes:      Range:
+            //float    f            Single          4           -3.4*10^38 to 3.4*10^38
+            //double   d            Double          8           ???
+            //decimal  m            Decimal         16          -7.9*10^28 to 7.9*10^28
+
+            //Character:            .Net Type:      Bytes:      Range:
+            //char                  Char            2           Unicode Characters
+
+            //Boolean:              /Net Type:      Bytes:      Range:
+            //bool                  Boolean         1           True / False
+
+            float number = 1.2f;
+            decimal number2 = 1.2m;
+        }
+    }
+}
